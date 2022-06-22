d485556066539548290d5aba2b7adcdc5b66d3f8
louyl <louyl@wedoctor.com>
2022-06-22 11:48:19 +0800

    update

--- packages/test-react-app/src/features/health/HealthDetail.tsx
+++ packages/test-react-app/src/features/health/HealthDetail.tsx
@@ -7,25 +7,21 @@ const HealthDetail: React.FC = function () {
   return (
     <main style={{ padding: 16 }}>
       <Portlet title="体检指标" description="(2022-06-22)">
-        <section>
-          <ListItem title="血糖" description={`正常范围在 3.9~6.1`}>
-            <Indicator value={3.1} max={6.1} min={3.9} />
-          </ListItem>
-          <ListItem title="收缩压">
-            <Indicator value={120} max={140} min={100} />
-          </ListItem>
-          <ListItem title="舒张压">
-            <Indicator value={75} max={90} min={60} />
-          </ListItem>
-        </section>
+        <ListItem title="血糖" description={`正常范围在 3.9~6.1`}>
+          <Indicator value={3.1} max={6.1} min={3.9} />
+        </ListItem>
+        <ListItem title="收缩压">
+          <Indicator value={120} max={140} min={100} />
+        </ListItem>
+        <ListItem title="舒张压">
+          <Indicator value={75} max={90} min={60} />
+        </ListItem>
       </Portlet>
 
       <Portlet title="指标范围设置">
-        <section>
-          <ListItem title="范围设置" description={`正常范围在 3.9~6.1`}>
-            <Indicator value={3.1} max={6.1} min={3.9} />
-          </ListItem>
-        </section>
+        <ListItem title="范围设置" description={`正常范围在 3.9~6.1`}>
+          <Indicator value={3.1} max={6.1} min={3.9} />
+        </ListItem>
       </Portlet>
     </main>
   );
